e27607d93bb6e753247f7b039d730806bba965ac
Mikhail Shuvarov <shyvarov@gmail.com>
2020-03-04 11:00:19 +0300

    feat: add gulp-sass in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,36 @@
 const { src, dest, watch } = require('gulp'),
   rename = require('gulp-rename'),
   cssnano = require('gulp-cssnano'),
+  sass = require('gulp-sass'),
   browserSync = require('browser-sync').create();
 
-function miniCSS () {
+
+function minCSS() {
   return src('src/css/style.css')
     .pipe(cssnano())
     .pipe(rename({suffix: '.min'}))
     .pipe(dest('src/css'))
 }
 
-function BrowserSync () {
+function serveSass() {
+  return src('src/sass/**/*.{scss,sass}')
+      .pipe(sass())
+      .pipe(dest('src/css'))
+      .pipe(browserSync.stream());
+}
+
+function BrowserSync() {
+  serveSass();
   browserSync.init({
     server: {
         baseDir: "src"
     }
   });
-  watch("src/*.html").on('change', browserSync.reload);
+  watch('src/*.html').on('change', browserSync.reload);
+  watch('src/sass/**/*.{scss,sass}', serveSass);
+  watch('src/js/**/*.js').on('change', browserSync.reload);
 }
 
-exports.miniCSS = miniCSS;
-exports.BrowserSync = BrowserSync;
+
+exports.minCSS = minCSS;
+exports.serve = BrowserSync;

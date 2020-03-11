a0c9eab6f9c7e79eddd546e09ec184629340edfe
Mikhail Shuvarov <shyvarov@gmail.com>
2020-03-11 22:25:54 +0300

    feat: add js function close for module & pixel perfect modal

--- docs/js/main.js
+++ docs/js/main.js
@@ -2,14 +2,36 @@ document.addEventListener("DOMContentLoaded", event => {
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close');
+  const modalDialog = document.querySelector('.modal__dialog');
   const switchModal = () => {
     modal.classList.toggle('modal--visible');
   };
+
 
   modalBtn.forEach(element => {
     element.addEventListener('click', switchModal);
   });
 
-  closeBtn.addEventListener('click', switchModal)
+  closeBtn.addEventListener('click', switchModal);
+
+
+  document.addEventListener('keydown', event => {
+    if(event.key == 'Escape' && modal.classList.contains('modal--visible')) {
+      switchModal();
+    }
+  });
+
+  modal.addEventListener('click', event => {
+    const target = event.target;
+    const itsModalDialog = target == modalDialog || modalDialog.contains(target);
+    const itsCloseBtn = target == closeBtn;
+    const modalActive = modal.classList.contains('modal--visible');
+
+    if (!itsModalDialog && !itsCloseBtn && modalActive) {
+      switchModal();
+    }
+  });
 
 });
+
+

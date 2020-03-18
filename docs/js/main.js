6e1178fb08d6a99a365be97a0b40642b54134cc1
Mikhail Shuvarov <shyvarov@gmail.com>
2020-03-18 17:02:57 +0300

    feat: add map section & our-client section

--- docs/js/main.js
+++ docs/js/main.js
@@ -307,4 +307,39 @@ $('.footer__form').validate({
 // Маска для телефона
 $('[type=tel]').mask('+7 (000) 000-00-00', {placeholder: "+7 (___) ___-__-__"});
 
+
+//Карта Yandex
+    ymaps.ready(function () {
+      var myMap = new ymaps.Map('map', {
+              center: [47.244722, 39.723183],
+              zoom: 17
+          }, {
+              searchControlProvider: 'yandex#search'
+          }),
+
+          // Создаём макет содержимого.
+          MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+              '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+          ),
+
+          myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+              hintContent: 'Наш офис',
+              balloonContent: 'Вход с ул. Нансена'
+          }, {
+              // Опции.
+              // Необходимо указать данный тип макета.
+              iconLayout: 'default#image',
+              // Своё изображение иконки метки.
+              iconImageHref: '../img/marker.png',
+              // Размеры метки.
+              iconImageSize: [32, 32],
+              // Смещение левого верхнего угла иконки относительно
+              // её "ножки" (точки привязки).
+              iconImageOffset: [-16, -32]
+          });
+
+      myMap.geoObjects
+          .add(myPlacemark);
+  });
+
 });

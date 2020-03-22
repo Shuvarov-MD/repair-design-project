a7a9302c38b2d45a2c0c4cb5fde9b59d82a4c3ca
Mikhail Shuvarov <shyvarov@gmail.com>
2020-03-22 19:34:16 +0300

    feat: add measure section

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,43 +1,5 @@
 !function(a){a.fn.viewportChecker=function(b){var c={classToAdd:"visible",classToRemove:"invisible",classToAddForFullView:"full-visible",removeClassAfterAnimation:!1,offset:100,repeat:!1,invertBottomOffset:!0,callbackFunction:function(a,b){},scrollHorizontal:!1,scrollBox:window};a.extend(c,b);var d=this,e={height:a(c.scrollBox).height(),width:a(c.scrollBox).width()};return this.checkElements=function(){var b,f;c.scrollHorizontal?(b=Math.max(a("html").scrollLeft(),a("body").scrollLeft(),a(window).scrollLeft()),f=b+e.width):(b=Math.max(a("html").scrollTop(),a("body").scrollTop(),a(window).scrollTop()),f=b+e.height),d.each(function(){var d=a(this),g={},h={};if(d.data("vp-add-class")&&(h.classToAdd=d.data("vp-add-class")),d.data("vp-remove-class")&&(h.classToRemove=d.data("vp-remove-class")),d.data("vp-add-class-full-view")&&(h.classToAddForFullView=d.data("vp-add-class-full-view")),d.data("vp-keep-add-class")&&(h.removeClassAfterAnimation=d.data("vp-remove-after-animation")),d.data("vp-offset")&&(h.offset=d.data("vp-offset")),d.data("vp-repeat")&&(h.repeat=d.data("vp-repeat")),d.data("vp-scrollHorizontal")&&(h.scrollHorizontal=d.data("vp-scrollHorizontal")),d.data("vp-invertBottomOffset")&&(h.scrollHorizontal=d.data("vp-invertBottomOffset")),a.extend(g,c),a.extend(g,h),!d.data("vp-animated")||g.repeat){String(g.offset).indexOf("%")>0&&(g.offset=parseInt(g.offset)/100*e.height);var i=g.scrollHorizontal?d.offset().left:d.offset().top,j=g.scrollHorizontal?i+d.width():i+d.height(),k=Math.round(i)+g.offset,l=g.scrollHorizontal?k+d.width():k+d.height();g.invertBottomOffset&&(l-=2*g.offset),k<f&&l>b?(d.removeClass(g.classToRemove),d.addClass(g.classToAdd),g.callbackFunction(d,"add"),j<=f&&i>=b?d.addClass(g.classToAddForFullView):d.removeClass(g.classToAddForFullView),d.data("vp-animated",!0),g.removeClassAfterAnimation&&d.one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",function(){d.removeClass(g.classToAdd)})):d.hasClass(g.classToAdd)&&g.repeat&&(d.removeClass(g.classToAdd+" "+g.classToAddForFullView),g.callbackFunction(d,"remove"),d.data("vp-animated",!1))}})},("ontouchstart"in window||"onmsgesturechange"in window)&&a(document).bind("touchmove MSPointerMove pointermove",this.checkElements),a(c.scrollBox).bind("load scroll",this.checkElements),a(window).resize(function(b){e={height:a(c.scrollBox).height(),width:a(c.scrollBox).width()},d.checkElements()}),this.checkElements(),this}}(jQuery);
 
-/*document.addEventListener("DOMContentLoaded", event => {
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-  const closeBtn = document.querySelector('.modal__close');
-  const modalDialog = document.querySelector('.modal__dialog');
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible');
-  };
-
-
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
-  });
-
-  closeBtn.addEventListener('click', switchModal);
-
-
-  document.addEventListener('keydown', event => {
-    if(event.key == 'Escape' && modal.classList.contains('modal--visible')) {
-      switchModal();
-    }
-  });
-
-  modal.addEventListener('click', event => {
-    const target = event.target;
-    const itsModalDialog = target == modalDialog || modalDialog.contains(target);
-    const itsCloseBtn = target == closeBtn;
-    const modalActive = modal.classList.contains('modal--visible');
-
-    if (!itsModalDialog && !itsCloseBtn && modalActive) {
-      switchModal();
-    }
-  });
-
-});*/
-
-
-
 $(document).ready(function () {
 
 //Анимация с помощью JS
@@ -127,7 +89,7 @@ $(document).ready(function () {
 
   bullets.css('left', prev.width() + 30);
   next.css('left', prev.width() + 25 + bullets.width() + 30);
-
+/*
   function mediaSize() {
     var swiperСontainer = $('.projects__swiper-container');
 		if (window.matchMedia('(max-width: 992px)').matches) {
@@ -149,7 +111,7 @@ $(document).ready(function () {
   mediaSize();
 	window.addEventListener('resize', mediaSize, false);
 
-
+*/
   var mySwiperSteps = new Swiper ('.steps__swiper-container', {
     loop: true,
     pagination: {
